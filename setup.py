fcb287bdf01ab504888925ca9fce85900061de1c
Valerian Saliou <valerian@valeriansaliou.name>
2016-07-10 22:11:56 +0200

    Bump to self namespace

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 import os
 from setuptools import setup, find_packages
- 
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
- 
+
 setup(
     name = "waaave-hitcount",
     version = "0.6.4",
-    url = 'http://github.com/waaave/waaave-hitcount',
+    url = 'http://github.com/valeriansaliou/waaave-hitcount',
     license = 'BSD',
     description = "Django hit counter application that tracks the number of hits/views for chosen objects",
     long_description = read('README.md'),
- 
+
     author = 'Damon Timm',
     author_email = 'damontimm@gmail.com',
- 
+
     packages = find_packages(),
-    
+
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Plugins',
